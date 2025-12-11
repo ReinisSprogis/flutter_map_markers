27575922c129b9b40c66214454bc031b606195dc
Reinis Sprogis <reinissprogis@gmail.com>
2025-12-12 00:24:39 +0200

    Add ball and raindrop marker path methods to MarkerPresets class

--- lib/marker_presets/marker_presets.dart
+++ lib/marker_presets/marker_presets.dart
@@ -2,7 +2,8 @@ import 'dart:math';
 import 'dart:ui';
 
 class MarkerPresets {
-    static (Path path, Offset arcCenter) ballMarkerPath(Offset center, {double ballRadius = 25, double knobHeight = 15, double knobAngle = pi / 4}) {
+  ///Returns a ball shaped Path and the center of the ball.
+  static (Path path, Offset arcCenter) ballMarkerPath(Offset center, {double ballRadius = 25, double knobHeight = 15, double knobAngle = pi / 4}) {
     final double halfAngle = knobAngle / 2;
 
     // Bottom tip of the knob
@@ -44,5 +45,54 @@ class MarkerPresets {
       ..close();
 
     return (path, arcCenter);
+  }
+
+  ///Returns a raindrop shaped Path and the center of the circular part of the raindrop.
+  static (Path path, Offset center) raindropMarkerPath(Offset bottom, {double radius = 20}) {
+    // Circle center is 2 radius above the final bottom anchor.
+    final cx = bottom.dx;
+    final cy = bottom.dy - radius * 2;
+
+    const kappa = 0.5522847498307936;
+    final k = kappa * radius;
+
+    // Actual circle anchors before deformation
+    final pTop = Offset(cx, cy - radius);
+    final pRight = Offset(cx + radius, cy);
+    final pLeft = Offset(cx - radius, cy);
+
+    // Deformed bottom = provided bottom
+    final pDropBottom = bottom;
+
+    // Control points
+    final cTR1 = Offset(cx + k, cy - radius);
+    final cTR2 = Offset(cx + radius, cy - k);
+
+    final cRB1 = Offset(cx + radius, cy + k);
+    final cRB2 = Offset(cx + k, cy + radius - (radius / 3));
+
+    final cBL1 = Offset(cx - k, cy + radius - (radius / 3));
+    final cBL2 = Offset(cx - radius, cy + k);
+
+    final cLT1 = Offset(cx - radius, cy - k);
+    final cLT2 = Offset(cx - k, cy - radius);
+
+    final path = Path()..moveTo(pTop.dx, pTop.dy);
+
+    // Top → Right
+    path.cubicTo(cTR1.dx, cTR1.dy, cTR2.dx, cTR2.dy, pRight.dx, pRight.dy);
+
+    // Right → Deformed Bottom
+    path.cubicTo(cRB1.dx, cRB1.dy, cRB2.dx, cRB2.dy, pDropBottom.dx, pDropBottom.dy);
+
+    // Bottom → Left
+    path.cubicTo(cBL1.dx, cBL1.dy, cBL2.dx, cBL2.dy, pLeft.dx, pLeft.dy);
+
+    // Left → Top
+    path.cubicTo(cLT1.dx, cLT1.dy, cLT2.dx, cLT2.dy, pTop.dx, pTop.dy);
+
+    path.close();
+
+    return (path, Offset(cx, cy));
   }
 }
