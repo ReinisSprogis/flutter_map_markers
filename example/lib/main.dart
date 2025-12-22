fa44bf1ae2b30eae45292bcbbdf47376a415ee05
Reinis Sprogis <reinissprogis@gmail.com>
2025-12-22 21:49:49 +0200

    Fix import path for marker demo page in main.dart

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,5 +1,5 @@
-import 'package:example/demo_pages/marker_demo_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_map_markers_example/demo_pages/marker_demo_page.dart';
 
 void main() {
   runApp(const MarkerDemoApp());
