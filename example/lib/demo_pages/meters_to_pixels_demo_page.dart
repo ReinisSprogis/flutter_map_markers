28286aec7df9222aba6602b759c18db1d87fac28
Reinis Sprogis <reinissprogis@gmail.com>
2025-12-22 21:51:50 +0200

    Fix import path for app_drawer.dart in meters_to_pixels_demo_page.dart

--- example/lib/demo_pages/meters_to_pixels_demo_page.dart
+++ example/lib/demo_pages/meters_to_pixels_demo_page.dart
@@ -1,8 +1,8 @@
-import 'package:example/app_drawer.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:flutter_map_markers/canvas_marker_layer/canvas_marker.dart';
 import 'package:flutter_map_markers/canvas_marker_layer/canvas_marker_layer.dart';
+import 'package:flutter_map_markers_example/app_drawer.dart';
 import 'package:latlong2/latlong.dart';
 
 /// Demonstration of markers that visualize a radius in meters converted to pixels.
