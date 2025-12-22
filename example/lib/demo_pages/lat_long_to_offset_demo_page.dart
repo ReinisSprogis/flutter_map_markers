aeb3159e1c0490663d0cc33a06f51f249d1ddd3f
Reinis Sprogis <reinissprogis@gmail.com>
2025-12-22 21:51:41 +0200

    Fix import paths for app_drawer.dart and utility.dart in lat_long_to_offset_demo_page.dart

--- example/lib/demo_pages/lat_long_to_offset_demo_page.dart
+++ example/lib/demo_pages/lat_long_to_offset_demo_page.dart
@@ -1,11 +1,11 @@
 import 'dart:math';
 
-import 'package:example/app_drawer.dart';
-import 'package:example/utility/utility.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:flutter_map_markers/canvas_marker_layer/canvas_marker.dart';
 import 'package:flutter_map_markers/canvas_marker_layer/canvas_marker_layer.dart';
+import 'package:flutter_map_markers_example/app_drawer.dart';
+import 'package:flutter_map_markers_example/utility/utility.dart';
 import 'package:latlong2/latlong.dart';
 
 /// Demonstration of LatLng to Offset conversion by drawing lines between markers.
