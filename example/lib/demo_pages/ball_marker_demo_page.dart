21aabf9d02de77eb060d8fa0928c5912aa03d8c8
Reinis Sprogis <reinissprogis@gmail.com>
2025-12-22 21:51:32 +0200

    Fix import path for app_drawer.dart in ball_marker_demo_page.dart

--- example/lib/demo_pages/ball_marker_demo_page.dart
+++ example/lib/demo_pages/ball_marker_demo_page.dart
@@ -1,9 +1,9 @@
-import 'package:example/app_drawer.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:flutter_map_markers/canvas_marker_layer/canvas_marker.dart';
 import 'package:flutter_map_markers/canvas_marker_layer/canvas_marker_layer.dart';
 import 'package:flutter_map_markers/marker_presets/marker_presets.dart';
+import 'package:flutter_map_markers_example/app_drawer.dart';
 import 'package:latlong2/latlong.dart';
 
 /// A demo page showcasing ball-shaped markers on a Flutter map.
